a34103d22e0a0b231bffa025b41808e93e726aa2
CcOoo0 <942423373@qq.com>
2024-09-12 10:33:13 +0800

    ban.js

--- yanchi.js
+++ yanchi.js
@@ -1,10 +1,6 @@
-// 延迟函数
-function delay(time) {
-    return new Promise(resolve => setTimeout(resolve, time));
+let url = $request.url;
+if (url.indexOf("nj.cschannel.anticheatexpert.com") !== -1) {
+    // 当检测到 nj.cschannel.anticheatexpert.com 的请求时，修改策略，放行 down.anticheatexpert.com
+    $policy.update("reject-group", "direct");
 }
-
-(async () => {
-    // 延迟3分钟，即180000毫秒
-    await delay(180000);  
-    $done({});
-})();
+$done({});
